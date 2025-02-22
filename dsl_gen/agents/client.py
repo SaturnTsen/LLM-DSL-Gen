308fe72c003066ecc4bb291340b51f8cd6f7c9cd
yimingchen <83628655+SaturnTsen@users.noreply.github.com>
2025-02-22 20:46:52 +0100

    Update client.py

--- dsl_gen/agents/client.py
+++ dsl_gen/agents/client.py
@@ -9,14 +9,6 @@ import logging
 
 logger = logging.getLogger('dsl_gen')
 
-_llm_clients = {
-    'openai': None,
-    'ollama': None,
-    'deepseek': None,
-    'mistralai': None,
-}
-
-# For MistralAI version, replace every "open" by "mistral" in the following code
 
 def getClient(backend, temperature) -> ChatOpenAI:
 
@@ -24,25 +16,18 @@ def getClient(backend, temperature) -> ChatOpenAI:
         'openai': API.MODEL_CFG.OpenAI,
         'ollama': API.MODEL_CFG.Ollama,
         'deepseek': API.MODEL_CFG.DeepSeek,
+        'mistralai': API.MODEL_CFG.MistralAI
     }[backend]
 
     logger.info("Model loaded: %s, base_url: %s", cfg.model, cfg.base_url)
 
-    assert backend in _llm_clients, f"Invalid backend: {backend}"
+    kwargs = {
+        'model': cfg.model,
+        'openai_api_base': cfg.base_url,
+        'openai_api_key': cfg.api_key,
+        'temperature': temperature
+    }
 
-    if _llm_clients[backend] is None:
-        cfg = {
-            'openai': API.MODEL_CFG.OpenAI,
-            'ollama': API.MODEL_CFG.Ollama,
-            'deepseek': API.MODEL_CFG.DeepSeek,
-            'mistralai': API.MODEL_CFG.MistralAI
-        }[backend]
-
-        logger.info("Model loaded: %s, base_url: %s", cfg.model, cfg.base_url)
-
-    return ChatOpenAI(
-        model=cfg.model,
-        openai_api_base=cfg.base_url,
-        openai_api_key=cfg.api_key,
-        temperature=temperature
-    )
+    return (ChatMistralAI(**kwargs)
+            if backend == 'mistralai'
+            else ChatOpenAI(**kwargs))
